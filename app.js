e8875e644219f715a625da40577873b685f9a9d9
sali <poshshazia@bdiplus.com>
2020-07-19 17:17:42 +0530

    demo app using object and default parameter

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const addMovie = document.getElementById('add-movie-btn');
-const search = document.getElementById('search-btn');
+const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
@@ -8,20 +8,57 @@ const captureMovie = () => {
     const getExtraInfo = document.getElementById('extra-name').value;
     const getExtraInfoValue = document.getElementById('extra-value').value;
 
-    if ( getExtraInfoValue.trim() === "" || getExtraInfo.trim() === "" || getMovie.trim() ===""){
+    if (getExtraInfoValue.trim() === "" || getExtraInfo.trim() === "" || getMovie.trim() === "") {
         return;
     }
 
     const movieCapturedFromUser = {
-       info :
-       { getMovie ,
-        [getExtraInfo] : getExtraInfoValue
-       },
-       id : Math.random()
+        info:
+        {
+            title: getMovie,
+            [getExtraInfo]: getExtraInfoValue
+        },
+        id: Math.random()
     };
 
     movies.push(movieCapturedFromUser);
     console.log(movieCapturedFromUser);
-} 
+    renderMovies()
+
+}
+
+const renderMovies = (filter = '') => {
+    const movieList = document.getElementById('movie-list');
+    if (movies.length === 0) {
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible');
+    }
+    movieList.innerHTML = '';
+
+    const filteredMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter))
+
+    filteredMovies.forEach((movie) => {
+        const movieEl = document.createElement('li');
+        let text = movie.info.title + ' - ';
+        for (const key in movie.info) {
+            console.log(key);
+            if (key !== 'title') {
+                console.log(key);
+                console.log(movie.info[key]);
+                text = text + `${key}: ${movie.info[key]}`;
+            }
+        }
+        movieEl.textContent = text;
+        movieList.append(movieEl);
+    });
+}
+
+const filteredMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovies(filterTerm);
+}
 
 addMovie.addEventListener('click', captureMovie);
+searchBtn.addEventListener('click', filteredMovieHandler);
